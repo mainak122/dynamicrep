946f4617c7a7c3b11af940188d65c089295aff23
Mainak Mandal <128247780+mainak122@users.noreply.github.com>
2023-04-10 15:54:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello')
+  res.send('Hello, This is my First Javascript File')
 })
 
 app.listen(4000, ()=>{
